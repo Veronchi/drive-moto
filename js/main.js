5d58f7980728abba1cbfa74b98d6ac967195ade3
Veronchi <zhigunova1995@gmail.com>
2020-11-26 14:45:55 +0300

    added slider

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow:'<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="img/arrow-left.svg" alt=""></button>',
+        nextArrow:'<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="img/arrow-right.svg" alt=""></button>',
+    });
+
+});
