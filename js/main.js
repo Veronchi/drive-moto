021482b07a18b2373bf66b0f90ecdaeb52d81944
Veronchi <zhigunova1995@gmail.com>
2020-12-11 21:57:35 +0300

    started filter

--- js/main.js
+++ js/main.js
@@ -29,5 +29,11 @@ $(function(){
     });
 
 
+    $('.filter-style').styler();
+
+    $('.filter__item-drop').on('click', function(){
+        $(this).toggleClass('filter__item-drop--active');
+        $(this).next().slideToggle('200');
+    });
 
 });
